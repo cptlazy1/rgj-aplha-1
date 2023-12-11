b8bb4ac2db0c36024d2789a437230e6c0561905d
cptlazy1 <o.culhaci@gmail.com>
2023-12-11 15:38:21 +0100

    Add validation to all fieldsin  game dto

--- src/main/java/com/example/rgjalpha1/dto/GameDto.java
+++ src/main/java/com/example/rgjalpha1/dto/GameDto.java
@@ -1,8 +1,6 @@
 package com.example.rgjalpha1.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,16 +12,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class GameDto {
 
-    // Todo: add validation for all fields
+
     public Long gameID;
 
     @Size(min = 1, max = 50, message = "Game name must be between 1 and 50 characters")
     public String gameName;
+
     @Min(value = 1000, message = "Year of release must be 4 digits")
     @Max(value = 9999, message = "Year of release must be 4 digits")
     public Integer gameYearOfRelease;
+
+    @Size(min = 1, max = 50, message = "Publisher name must be between 1 and 50 characters")
     public String gamePublisher;
+
+    @NotNull(message = "Game is original must be true or false")
     public Boolean gameIsOriginal;
+
+    @Size(min = 1, max = 50, message = "System name must be between 1 and 50 characters")
     public String systemName;
 
 }
