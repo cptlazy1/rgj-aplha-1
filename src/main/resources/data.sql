c00181eb807e672c22c18acf3419b8fa99a30319
cptlazy1 <o.culhaci@gmail.com>
2024-01-11 10:58:54 +0100

    Add usernames to all games and systems for frontend development

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,24 +10,24 @@ INSERT INTO users (is_enabled, email, password, role, username)
 VALUES (true, 'tik@groot.com', '$2a$12$IzA1Ja1LH4PSMoro9PeITO1etDlknPjSX1nLusgt1vi9c1uaEXdEK', 'ADMIN', 'trekkie76');
 
 -- Games
-INSERT INTO games (game_name, game_year_of_release, game_publisher, game_is_original, system_name)
-VALUES ('Sonic', 1991, 'Sega', true, 'MegaDrive');
+INSERT INTO games (game_name, game_year_of_release, game_publisher, game_is_original, system_name, user_name)
+VALUES ('Sonic', 1991, 'Sega', true, 'MegaDrive', 'porgy123');
 
-INSERT INTO games (game_name, game_year_of_release, game_publisher, game_is_original, system_name)
-VALUES ('Samurai ShoDown 2', 1994, 'SNK', true, 'Neo-Geo');
+INSERT INTO games (game_name, game_year_of_release, game_publisher, game_is_original, system_name, user_name)
+VALUES ('Samurai ShoDown 2', 1994, 'SNK', true, 'Neo-Geo', 'porgy123');
 
-INSERT INTO games (game_name, game_year_of_release, game_publisher, game_is_original, system_name)
-VALUES ('Mario Bros 3', 1988, 'Nintendo', false, 'NES');
+INSERT INTO games (game_name, game_year_of_release, game_publisher, game_is_original, system_name, user_name)
+VALUES ('Mario Bros 3', 1988, 'Nintendo', false, 'NES', 'porgy123');
 
 --  Game systems
-INSERT INTO game_systems (game_system_name, game_system_brand, game_system_year_of_release, is_ready_to_play)
-VALUES ('Dreamcast', 'SEGA', 1999, true);
+INSERT INTO game_systems (game_system_name, game_system_brand, game_system_year_of_release, is_ready_to_play, user_name)
+VALUES ('Dreamcast', 'SEGA', 1999, true, 'porgy123');
 
-INSERT INTO game_systems (game_system_name, game_system_brand, game_system_year_of_release, is_ready_to_play)
-VALUES ('Neo-Geo', 'SNK', 1990, true);
+INSERT INTO game_systems (game_system_name, game_system_brand, game_system_year_of_release, is_ready_to_play, user_name)
+VALUES ('Neo-Geo', 'SNK', 1990, true, 'porgy123');
 
-INSERT INTO game_systems (game_system_name, game_system_brand, game_system_year_of_release, is_ready_to_play)
-VALUES ('Super Nintendo', 'Nintendo', 1990, true);
+INSERT INTO game_systems (game_system_name, game_system_brand, game_system_year_of_release, is_ready_to_play, user_name)
+VALUES ('Super Nintendo', 'Nintendo', 1990, true, 'porgy123');
 
 -- Game conditions
 INSERT INTO game_conditions (has_case, has_manual, has_scratches, has_stickers, has_writing)
