4e5d9c4bbd3b2ee0aaf22d462d823259a83af50e
cptlazy1 <o.culhaci@gmail.com>
2024-01-24 13:40:03 +0100

    Change validity of jw token to 48 hours

--- src/main/java/com/example/rgjalpha1/security/JwtService.java
+++ src/main/java/com/example/rgjalpha1/security/JwtService.java
@@ -36,7 +36,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 6)) // token valid for 6 hours
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 48)) // token valid for 48 hours
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
