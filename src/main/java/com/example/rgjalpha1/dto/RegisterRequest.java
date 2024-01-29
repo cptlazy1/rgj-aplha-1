14b56839b36af7c908c0bdc3e0e0e9a17fc6d01e
cptlazy1 <o.culhaci@gmail.com>
2024-01-29 14:59:25 +0100

    Add extra validation to username. Only lowercase letters, numbers, _ and - are allowed

--- src/main/java/com/example/rgjalpha1/dto/RegisterRequest.java
+++ src/main/java/com/example/rgjalpha1/dto/RegisterRequest.java
@@ -4,6 +4,7 @@ package com.example.rgjalpha1.dto;
 import com.example.rgjalpha1.validation.UniqueUsername;
 import com.example.rgjalpha1.validation.ValidEmail;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,7 @@ public class RegisterRequest {
 
     @Size(min = 8, max = 20 , message = "Username must be between 8 and 20 characters")
     @UniqueUsername
+    @Pattern(regexp = "^[a-z0-9_-]*$", message = "Username must contain only lowercase letters, numbers, underscore and hyphen")
     private String username;
 
     @Size(min = 8, max = 16, message = "Password must be between 8 and 16 characters")
